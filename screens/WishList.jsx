407daf68938731f103fd5653ec636adf8c6c2f7a
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2023-04-20 18:02:24 +0200

    Add SaveButton component, Add header and notes container for WishList screen

--- screens/WishList.jsx
+++ screens/WishList.jsx
@@ -1,17 +1,25 @@
-import { StyleSheet, Text, View, TextInput, ImageBackground, Pressable, ScrollView } from 'react-native';
-import MainButton from '../components/MainButton';
-import Button from '../components/Button';
-import bgImg from '../assets/images/bg.png';
-import { useCallback } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ImageBackground,
+  Pressable,
+  ScrollView,
+} from 'react-native';
+import { useCallback, useState } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import SummaryHeader from '../components/SummaryHeader';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function WishList({ navigation }) {
+  const [text, setText] = useState('');
+
   const [fontsLoaded] = useFonts({
-    'AbhayaLibre': require('../assets/fonts/AbhayaLibre-Bold.ttf'),
-    'QwitcherGrypen': require('../assets/fonts/QwitcherGrypen-Bold.ttf'),
+    AbhayaLibre: require('../assets/fonts/AbhayaLibre-Bold.ttf'),
+    QwitcherGrypen: require('../assets/fonts/QwitcherGrypen-Bold.ttf'),
   });
 
   const onLayoutRootView = useCallback(async () => {
@@ -24,12 +32,12 @@ export default function WishList({ navigation }) {
     return null;
   }
 
-
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-        
-        <MainButton title="WishList" style={{height: 60, Color: 'black', fontFamily: 'AbhayaLibre', backgroundColor:'#bdbdbd'}} />
+    <View style={styles.container}>
+      <SummaryHeader title={'WISH LIST'} />
+      <View style={styles.notesBlock}>
 
+      </View>
     </View>
   );
 }
@@ -37,10 +45,13 @@ export default function WishList({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#b0b0b0',
-    justifyContent: 'center',
     alignItems: 'center',
-    width: "100%", 
+    justifyContent: 'center',
+  },
+  notesBlock: {
+    width: '85%',
+    flex: 4,
+    backgroundColor: 'rgba(196, 157, 98, 0.85)',
+    marginBottom: 20,
   },
 });
-
