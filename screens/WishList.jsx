2f733ba1956a1b01acfa3413bedb5d00cadf8b4f
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2023-05-04 11:25:02 +0200

    Add layout and functional multiline input

--- screens/WishList.jsx
+++ screens/WishList.jsx
@@ -1,12 +1,4 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  ImageBackground,
-  Pressable,
-  ScrollView,
-} from 'react-native';
+import { StyleSheet, View, TextInput, Text } from 'react-native';
 import { useCallback, useState } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
@@ -16,6 +8,8 @@ SplashScreen.preventAutoHideAsync();
 
 export default function WishList({ navigation }) {
   const [text, setText] = useState('');
+  const [inputHeight, setInputHeight] = useState(0);
+  const [containerHeight, setContainerHeight] = useState(0);
 
   const [fontsLoaded] = useFonts({
     AbhayaLibre: require('../assets/fonts/AbhayaLibre-Bold.ttf'),
@@ -32,11 +26,54 @@ export default function WishList({ navigation }) {
     return null;
   }
 
+  const handleContentSizeChange = (event) => {
+    setInputHeight(event.nativeEvent.contentSize.height);
+  };
+
+  const handleKeyPress = ({ nativeEvent }) => {
+    if (nativeEvent.key === 'Enter') {
+      setText(text + '\n');
+    }
+  };
+
+  const onContainerLayout = (event) => {
+    const { height } = event.nativeEvent.layout;
+    setContainerHeight(height);
+  };
+
+  const lineCount = Math.floor(containerHeight / 10);
+  const lines = Array.from({ length: lineCount }, (_, i) => i);
+
   return (
     <View style={styles.container}>
       <SummaryHeader title={'WISH LIST'} />
-      <View style={styles.notesBlock}>
-
+      <View style={styles.notesBlock} onLayout={onContainerLayout}>
+        <Text style={styles.notesHeader}>Notes</Text>
+        {lines.map((line) => (
+          <View
+            key={line}
+            style={[
+              styles.line,
+              {
+                height: 1,
+              },
+            ]}
+          />
+        ))}
+        <TextInput
+          style={[
+            styles.input,
+            {
+              height: Math.max(100, inputHeight),
+            },
+          ]}
+          multiline={true}
+          value={text}
+          onChangeText={(text) => setText(text)}
+          onContentSizeChange={handleContentSizeChange}
+          onKeyPress={handleKeyPress}
+          onSubmitEditing={() => setInputHeight(0)}
+        />
       </View>
     </View>
   );
@@ -48,10 +85,36 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  notesHeader: {
+    fontFamily: 'AbhayaLibre',
+    fontSize: 28,
+    color: 'white',
+    marginTop: 30,
+  },
   notesBlock: {
     width: '85%',
     flex: 4,
     backgroundColor: 'rgba(196, 157, 98, 0.85)',
     marginBottom: 20,
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    padding: 5,
+    textAlignVertical: 'top',
+    textAlign: 'center',
+    fontFamily: 'AbhayaLibre',
+    fontSize: 25,
+    color: 'white',
+    width: '90%',
+    lineHeight: 53,
+    position: 'absolute',
+    top: 80,
+  },
+  line: {
+    width: '90%',
+    backgroundColor: 'white',
+    marginVertical: 26,
+    top: 32,
   },
 });
